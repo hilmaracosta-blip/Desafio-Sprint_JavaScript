acfb5a20542e007507058da4298519baccb4a7bb
Hilmaracosta-blip <himara.costa@aln.senaicimatec.edu.br>
2026-05-15 14:50:58 -0300

    Implementando a classe Carousel e lógica de transição

--- js/carousel.js
+++ js/carousel.js
@@ -4,29 +4,62 @@
 
 //Array storage class
 let carouselArr = [];
+let currentIndex = 0;
+let timer; 
+
 
 
 //class Carousel
 class Carousel {
-
+constructor(image, title, url) {
+        this.image = image;
+        this.title = title;
+        this.url = url;
+    }
     
       
-    static Start(arr){
-        if(arr){
+    static Start(arr) {
+        carouselArr = arr;
+        this.ShowItem(); // Mostra o primeiro item
+        this.ResetTimer(); // Inicia o temporizador automático
+    }
 
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
-        } else {
-            throw "Method Start need a Array Variable.";
+    static ResetTimer() {
+        clearInterval(timer);
+        timer = setInterval(() => {
+            this.Next();
+        }, 2000);
+    }
+
+    static Next() {
+        currentIndex = (currentIndex + 1) % carouselArr.length;
+        this.ShowItem();
+        this.ResetTimer(); 
+    }
+
+    static Prev() {
+        // Vai para o anterior (e volta para o fim se estiver no início)
+        currentIndex = (currentIndex - 1 + carouselArr.length) % carouselArr.length;
+        this.ShowItem();
+        this.ResetTimer();
+    }
+
+    static ShowItem() {
+        const carouselDiv = document.getElementById("carousel");
+        const titleDiv = document.getElementById("carousel-title");
+
+        if (carouselArr.length > 0) {
+            const item = carouselArr[currentIndex];
+
+            // Atualiza a imagem e os estilos
+            carouselDiv.style.backgroundImage = `url('img/${item.image}')`;
+            carouselDiv.style.backgroundSize = "contain"; 
+            carouselDiv.style.backgroundRepeat = "no-repeat";
+            carouselDiv.style.backgroundPosition = "center";
+            carouselDiv.style.height = "860px";
+
+            // Atualiza o título e o link
+            titleDiv.innerHTML = `<a href="${item.url}" style="text-decoration: none; color: #003478;">${item.title}</a>`;
         }
     }
-
-    static Next(){
-        
-    }
-};
+}
