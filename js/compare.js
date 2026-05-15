351b0f902f39adeb04533c3b8398bfbd79a3d2ab
Hilmaracosta-blip <himara.costa@aln.senaicimatec.edu.br>
2026-05-15 15:00:37 -0300

    Implementando lógica de comparação de veículos

--- js/compare.js
+++ js/compare.js
@@ -1,52 +1,22 @@
 
-//car
-let carArr = [];
+
+let carsToCompare = [];
 
 class Car {
-   
-
-    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-       
+   constructor(modelo, preco, alturacacamba, alturaveiculo, alturasolo, capacidadecarga, motor, potencia, volumecacamba, roda, image) {
+        this.modelo = modelo;
+        this.preco = preco;
+        this.alturacacamba = alturacacamba;
+        this.alturaveiculo = alturaveiculo;
+        this.alturasolo = alturasolo;
+        this.capacidadecarga = capacidadecarga;
+        this.motor = motor;
+        this.potencia = potencia;
+        this.volumecacamba = volumecacamba;
+        this.roda = roda;
+        this.image = image;
     }
 } 
 
 // search on array if exist carClass returning 1 if not return -1
-function GetCarArrPosition(arr, carClass) {
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i].nome  === carClass.nome)
-            return i;
-    }
-    return -1;
-}
-
-function SetCarToCompare(el, carClass) {
-   
-    if(carClass instanceof Car){       
-        if(el.checked){
-                
-            
-        } else {
-          
-        } 
-    } else {
-        throw "You need set a Car Class";
-    }
-}
-
-function ShowCompare() {
-    if(carArr.length < 2) {
-        alert("Precisa marcar 2 carros para apresentar a comparação");
-        return;
-    }
-
-    UpdateCompareTable();
-    document.getElementById("compare").style.display = "block";
-}
-
-function HideCompare(){
-    document.getElementById("compare").style.display = "none"; 
-}
-
-function UpdateCompareTable() {
-    
 }
